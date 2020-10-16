171ae99bd79f21dc11089295413d8da3b827ffee
Jason Rasmussen <jrasm91@gmail.com>
2020-10-16 03:28:13 -0700

    chore: update dummy data

--- scripts/dummy_data.py
+++ scripts/dummy_data.py
@@ -27,8 +27,9 @@ base = [
     71,
 ]
 temp = 70
-with open('../data/pool_temp_history.csv', 'w') as f:
-    for hour in range(24):
-        for minute in [0, 15, 30, 45]:
-            temp = base[hour]
-            f.write('%s,2020-10-10T%02d:%02d:00\n' % (temp, hour, minute))
+with open('../data/air_temp_history.csv', 'w') as f:
+    for day in range(2):
+        for hour in range(24):
+            for minute in [0, 15, 30, 45]:
+                temp = base[hour] + 15
+                f.write('%s,2020-10-%02dT%02d:%02d:00\n' % (temp, day + 1, hour, minute))
